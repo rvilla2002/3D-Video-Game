30c13fda3071894c3b95f1e7774bf68d3b48a8c1
Luis <jaloaguero@gmail.com>
2023-04-26 21:55:45 -0500

    fixed camera problem, now should work

--- Assets/camera_script_master.cs
+++ Assets/camera_script_master.cs
@@ -2,38 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScriptMaster : MonoBehaviour
+public class camera_script_master : MonoBehaviour
 {
-    public GameObject player;
-    public float cameraSpeed = 5;
-    public float distance = 5;
-    public Vector2 pitchLimits = new Vector2(-20, 80);
+    public Transform target;
+    //controls x axis
+    public float distance = 250f;
+    //controls y axis, leave alone its finicky
+    public float height = 1000f;
+    //basically camera sensitivity
+    public float rotationDamping = 10f;
+    //how fast it zooms in and out, should not even need it
+    public float zoomSpeed = 20f;
+    //smallest allowed to be left it low dont zoom in that much it doesnt work
+    public float minZoomDistance = 2f;
+    //largest allowed to zoom out
+    public float maxZoomDistance = 20f;
+    public float minVerticalAngle = -80f;
+    public float maxVerticalAngle = 80f;
 
-    private float pitch = 0;
-    private float yaw = 0;
+    private float currentRotationAngle = 0f;
+    private float currentHeight = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Initialize pitch and yaw based on the current camera rotation
-        Vector3 eulerAngles = transform.eulerAngles;
-        pitch = eulerAngles.x;
-        yaw = eulerAngles.y;
+    void LateUpdate() {
+
+        //grabs input from the mouse and zoom the camera
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        distance -= scroll * zoomSpeed;
+        distance = Mathf.Clamp(distance, minZoomDistance, maxZoomDistance);
+
+        //grabs input from the mouse and rotate the camera
+        float rotation = Input.GetAxis("Mouse X");
+        currentRotationAngle += rotation * rotationDamping;
+        currentRotationAngle = Mathf.Repeat(currentRotationAngle, 360f);
+
+        //grabsinput form the mouse and tilt the camera
+        float verticalRotation = -Input.GetAxis("Mouse Y");
+        currentHeight = Mathf.Clamp(currentHeight + verticalRotation * rotationDamping, minVerticalAngle, maxVerticalAngle);
+
+        // Calculate the position and rotation of the camera
+        //i got this from the internet dont ask me how it works but it works
+        Vector3 targetPosition = target.position - Quaternion.Euler(currentHeight, currentRotationAngle, 0f) * Vector3.forward * distance;
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+
+        //basically updates the position of camera
+        transform.position = targetPosition;
+        transform.rotation = targetRotation;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Update pitch and yaw based on mouse input
-        yaw += cameraSpeed * Input.GetAxis("Mouse X");
-        pitch -= cameraSpeed * Input.GetAxis("Mouse Y");
-        pitch = Mathf.Clamp(pitch, pitchLimits.x, pitchLimits.y);
-
-        // Calculate the new camera position and rotation based on pitch, yaw, and distance
-        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
-        Vector3 direction = rotation * new Vector3(0, 0, -distance);
-        transform.position = player.transform.position + direction;
-        transform.LookAt(player.transform.position);
-    }
 }
-
