f119a9e063b631415ee1b5908e7f7f0e7da33dda
Luis <jaloaguero@gmail.com>
2023-05-06 23:50:09 -0500

    pause, menu, connected to other menus

--- Assets/camera_script_master.cs
+++ Assets/camera_script_master.cs
@@ -25,30 +25,33 @@ public class camera_script_master : MonoBehaviour
     private float currentHeight = 0f;
 
     void LateUpdate() {
+        
+        if(!PauseMenu.isPaused) {
+            //grabs input from the mouse and zoom the camera
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            distance -= scroll * zoomSpeed;
+            distance = Mathf.Clamp(distance, minZoomDistance, maxZoomDistance);
 
-        //grabs input from the mouse and zoom the camera
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        distance -= scroll * zoomSpeed;
-        distance = Mathf.Clamp(distance, minZoomDistance, maxZoomDistance);
+            //grabs input from the mouse and rotate the camera
+            float rotation = Input.GetAxis("Mouse X");
+            currentRotationAngle += rotation * rotationDamping;
+            currentRotationAngle = Mathf.Repeat(currentRotationAngle, 360f);
 
-        //grabs input from the mouse and rotate the camera
-        float rotation = Input.GetAxis("Mouse X");
-        currentRotationAngle += rotation * rotationDamping;
-        currentRotationAngle = Mathf.Repeat(currentRotationAngle, 360f);
+            //grabsinput form the mouse and tilt the camera
+            float verticalRotation = -Input.GetAxis("Mouse Y");
+            currentHeight = Mathf.Clamp(currentHeight + verticalRotation * rotationDamping, minVerticalAngle, maxVerticalAngle);
 
-        //grabsinput form the mouse and tilt the camera
-        float verticalRotation = -Input.GetAxis("Mouse Y");
-        currentHeight = Mathf.Clamp(currentHeight + verticalRotation * rotationDamping, minVerticalAngle, maxVerticalAngle);
+            // Calculate the position and rotation of the camera
+            //i got this from the internet dont ask me how it works but it works
+            Vector3 targetPosition = target.position - Quaternion.Euler(currentHeight, currentRotationAngle, 0f) * Vector3.forward * distance;
+            Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
 
-        // Calculate the position and rotation of the camera
-        //i got this from the internet dont ask me how it works but it works
-        Vector3 targetPosition = target.position - Quaternion.Euler(currentHeight, currentRotationAngle, 0f) * Vector3.forward * distance;
-        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
-
-        //basically updates the position of camera
-        transform.position = targetPosition;
-        transform.rotation = targetRotation;
-        transform.LookAt(target);
+            //basically updates the position of camera
+            transform.position = targetPosition;
+            transform.rotation = targetRotation;
+            transform.LookAt(target);
+        }
+        
     }
 
 }
--- Assets/player_master_script.cs
+++ Assets/player_master_script.cs
@@ -41,46 +41,48 @@ public class player_master_script : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        // Converting the mouse position to a point in 3D-space
-        Vector3 point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-        // Using some math to calculate the point of intersection between the line going through the camera and the mouse position with the XZ-Plane
-        float t = cam.transform.position.y / (cam.transform.position.y - point.y);
-        Vector3 finalPoint = new Vector3(t * (point.x - cam.transform.position.x) + cam.transform.position.x, 1, t * (point.z - cam.transform.position.z) + cam.transform.position.z);
-        // Rotating the object to that point
-        transform.LookAt(finalPoint, Vector3.up);
+    {   
+        if(!PauseMenu.isPaused) {
+            // Converting the mouse position to a point in 3D-space
+            Vector3 point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+            // Using some math to calculate the point of intersection between the line going through the camera and the mouse position with the XZ-Plane
+            float t = cam.transform.position.y / (cam.transform.position.y - point.y);
+            Vector3 finalPoint = new Vector3(t * (point.x - cam.transform.position.x) + cam.transform.position.x, 1, t * (point.z - cam.transform.position.z) + cam.transform.position.z);
+            // Rotating the object to that point
+            transform.LookAt(finalPoint, Vector3.up);
+            // Checks if any key was pressed
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * player_speed * Time.deltaTime);
+                animator.SetBool("isMoving", true);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * player_speed * Time.deltaTime);
+                animator.SetBool("isMoving", true);
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * player_speed * Time.deltaTime);
+                animator.SetBool("isMoving", true);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate((Vector3.forward * player_speed * Time.deltaTime) * -1);
+                animator.SetBool("isMoving", true);
+            }
+            else
+            {
+                // If no key was pressed, person not moving, so isMoving is false, to start idle animation.
+                animator.SetBool("isMoving", false);
+            }
 
-        // Checks if any key was pressed
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * player_speed * Time.deltaTime);
-            animator.SetBool("isMoving", true);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * player_speed * Time.deltaTime);
-            animator.SetBool("isMoving", true);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * player_speed * Time.deltaTime);
-            animator.SetBool("isMoving", true);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate((Vector3.forward * player_speed * Time.deltaTime) * -1);
-            animator.SetBool("isMoving", true);
-        }
-        else
-        {
-            // If no key was pressed, person not moving, so isMoving is false, to start idle animation.
-            animator.SetBool("isMoving", false);
+            if (Input.GetMouseButtonDown(0) && !isAttacking)
+            {
+                StartCoroutine(TriggerAttack());
+            }
         }
 
-        if (Input.GetMouseButtonDown(0) && !isAttacking)
-        {
-            StartCoroutine(TriggerAttack());
-        }
     }
 
     IEnumerator TriggerAttack()
@@ -96,7 +98,7 @@ public class player_master_script : MonoBehaviour
                 monster.TakeDamage(attackValue);
             }
         }
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.3f);
         animator.SetBool("isAttacking", false);
         isAttacking = false;
     }
